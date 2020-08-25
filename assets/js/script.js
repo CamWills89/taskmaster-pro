d455300e771e71e74f34c2d29af11b29da18b9c3
Cameron <cameron.wills89@outlook.com>
2020-08-25 11:07:01 -0500

    added drop and drag sorting features

--- assets/js/script.js
+++ assets/js/script.js
@@ -189,6 +189,73 @@ $("#remove-tasks").on("click", function () {
   saveTasks();
 });
 
+//creating sortable tasks
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    //array to store the task data in
+    var tempArr = [];
+
+    //loop over current set of childrea in sortable list
+    $(this).children().each(function(){
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      //add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    //making sure that the dragged tasks are saved and stored (in localStorage) in their new location
+    //trim down list's ID to match object property
+    var arrNamer = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    //update arra on tasks object and save
+    tasks[arrNamer] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log(("over"));
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
+
 // load tasks for the first time
 loadTasks();
 
