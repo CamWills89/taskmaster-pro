d3cbb86db5d4dc2e3c4c2580c57abf55d1114103
Cameron <cameron.wills89@outlook.com>
2020-08-25 22:24:01 -0500

    implemented task audit intervals and custom fonts

--- assets/js/script.js
+++ assets/js/script.js
@@ -301,7 +301,11 @@ var auditTask = function (taskEl) {
     $(taskEl).addClass("list-group-item-warning")
   }
 };
+setInterval(function() {
+  $(".card .list-group-item").each(function(el) {
+    auditTask(el)
+  });
+}, (1000 * 60) * 30);
+
 // load tasks for the first time
 loadTasks();
-
-
